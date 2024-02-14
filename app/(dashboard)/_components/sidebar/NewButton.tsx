8a38acc6b1154fb8c10bba24c3045d4d700474dd
muhuiyu <muhuiyu.grace@gmail.com>
2024-02-14 23:52:35 +0700

    Create navigation bar and side bar components

---
+++ app/(dashboard)/_components/sidebar/NewButton.tsx
@@ -0,0 +1,22 @@
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
+import { CreateOrganization } from '@clerk/nextjs'
+import { Plus } from 'lucide-react'
+
+const NewButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="aspect-square">
+          <button className="bg-white/25 h-full w-full rounded-md justify-center items-center flex opacity-60 hover:opacity-100 transition">
+            <Plus className="text-white" />
+          </button>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
+        <CreateOrganization />
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default NewButton
