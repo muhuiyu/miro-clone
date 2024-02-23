eca702c428184229dcb0455cb7a7a3c172cbfbf2
muhuiyu <muhuiyu.grace@gmail.com>
2024-02-23 19:18:01 +0800

    Change info to a client component

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -15,7 +15,7 @@ const Toolbar = () => {
   )
 }
 
-Toolbar.Skeleton = function ToolbarSkeleton() {
+export const ToolbarSkeleton = () => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 h-[360px] w-[52px] bg-white shadow-md rounded-md" />
   )
